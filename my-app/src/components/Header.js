ad3c3686bea36a78f5012dea488f17d19befc02d
Yakushin103 <VYakushin103@yandex.ru>
2020-01-06 17:49:12 +0300

    Add folder Components

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://avatars.mds.yandex.net/get-pdb/2212586/6dda1a62-c391-4e13-b314-93ac53dafa89/s1200" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ my-app/src/components/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://avatars.mds.yandex.net/get-pdb/2212586/6dda1a62-c391-4e13-b314-93ac53dafa89/s1200" />
+            </div>
+            Avatar
+            <div>
+                My post
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
